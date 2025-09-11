dae07ea3aa163a9777d16ff7aebfa9b1fcbd11b2
Shriti1210 <shritiyadav12@gmail.com>
2025-09-11 09:52:08 +0530

    Solved max consecutive 1s

---
+++ Leetcode/Arrays/Problem_485.cpp
@@ -0,0 +1,42 @@
+/*
+485. Max Consecutive Ones
+Given a binary array nums, return the maximum number of consecutive 1's in the array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,0,1,1,1]
+Output: 3
+Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+Example 2:
+
+Input: nums = [1,0,1,1,0,1]
+Output: 2
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+nums[i] is either 0 or 1.
+*/
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int n=nums.size();
+        int count=0;
+        int max_count=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==1){
+                count++;
+                max_count=max(count,max_count);
+            }else{
+                count=0;
+            }
+        }
+        return max_count;
+    }
+};
