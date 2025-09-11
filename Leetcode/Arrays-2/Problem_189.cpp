c8b6e265e2fea7aee88127a41b2d72b053c3d32a
Shriti1210 <shritiyadav12@gmail.com>
2025-09-11 09:48:02 +0530

    Solved Right rotate array Problem

--- Leetcode/Arrays-2/Problem_189.cpp
+++ Leetcode/Arrays-2/Problem_189.cpp
@@ -1,8 +1,6 @@
 /*189. Rotate Array
 Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
 
- 
-
 Example 1:
 
 Input: nums = [1,2,3,4,5,6,7], k = 3
@@ -32,3 +30,55 @@ Follow up:
 Try to come up with as many solutions as you can. There are at least three different ways to solve this problem.
 Could you do it in-place with O(1) extra space?
 */
+/*
+Brute Force Approach:
+First store the d elements first in a temporary array
+next move the remaining element on the left side 
+after moving the elements to the start,push the remaining d elements back in the array
+*/
+
+//LEFT ROTATE ARRAY PROGRAM
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+void leftRotate(vector<int>& nums, int k) {
+        int n=nums.size();
+        k=k%n;
+        vector<int>temp;
+        //put d elements in temp array
+        for(int i=0;i<k;i++){//O(d)
+            temp.push_back(nums[i]);
+        }
+        //Push array elements to the front of array
+        for(int i=k;i<n;i++){
+            nums[i-k]=nums[i];
+        }
+        //put temp array elements back in original
+        for(int i=n-k;i<n;i++){
+            nums[i]=temp[i-(n-k)];
+        }
+    
+}
+
+void rightRotate(vector<int>& nums, int k) {
+        int n=nums.size();
+        k=k%n;
+        vector<int>temp;
+        //put last d elements in temp array
+        for(int i=0;i<k;i++){//O(d)
+            temp.push_back(nums[n-k+i]);
+        }
+        //Push array elements to the back of the array
+         for (int i = n - k - 1; i >= 0; i--) {
+            nums[i + k] = nums[i];
+        }
+
+        // Step 3: put temp elements at front
+        for (int i = 0; i < k; i++) {
+            nums[i] = temp[i];
+        }
+    
+}
+
+//OPTIMIZED SOLUTION:use reverse the array concept and reverse the array 
+
